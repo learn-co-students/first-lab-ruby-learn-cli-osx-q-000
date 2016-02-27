af21ee6a705d493ddd72956945be74669436abc9
junots <nnilanon@gmail.com>
2016-02-27 11:20:32 -0600

    Done.

---
+++ spec/lab1.rb
@@ -0,0 +1,7 @@
+edited_file = edit-me.open("/Users/Nevin/Development/code/first-lab-ruby-learn-cli-osx-q-000/edit-me.txt", "r+")
+
+line1 = "i'm going to make this work."
+
+edited_file.write(line1)
+
+edited_file.close
